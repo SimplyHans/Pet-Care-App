805958db5ca6a520283e294b73f0bad0b10fd053
Hussein Bani Khaled <hansonhans88@gmail.com>
2025-10-02 18:31:00 -0400

    adding a setting page and add some drawable you can use later connecting login page to the main page

--- app/src/main/java/ca/gbc/petcareapp/LoginFragment.kt
+++ app/src/main/java/ca/gbc/petcareapp/LoginFragment.kt
@@ -2,6 +2,7 @@ package ca.gbc.petcareapp
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -15,5 +16,9 @@ class LoginFragment : Fragment(R.layout.activity_login) {
 
             findNavController().navigate(R.id.welcomeFragment)
         }
+        view.findViewById<com.google.android.material.button.MaterialButton>(R.id.button_login_login)?.setOnClickListener {
+            findNavController().navigate(R.id.action_login_to_welcome)
+        }
+
     }
 }
--- app/src/main/java/ca/gbc/petcareapp/WelcomeFragment.kt
+++ app/src/main/java/ca/gbc/petcareapp/WelcomeFragment.kt
@@ -10,7 +10,7 @@ class WelcomeFragment : Fragment(R.layout.activity_welcome) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Buttons from activity_welcome.xml
+
         view.findViewById<Button>(R.id.button_login)?.setOnClickListener {
             findNavController().navigate(R.id.action_welcome_to_login)
         }
@@ -18,5 +18,6 @@ class WelcomeFragment : Fragment(R.layout.activity_welcome) {
         view.findViewById<Button>(R.id.button_register)?.setOnClickListener {
             findNavController().navigate(R.id.action_welcome_to_register)
         }
+
     }
 }
